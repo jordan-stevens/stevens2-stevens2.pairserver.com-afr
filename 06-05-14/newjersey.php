e9c61e891d368af58dc46ede5879966b5ff95460
Jordan <jordan@stevens-stevens.com>
2014-06-05 11:27:52 -0400

    AFRCC
    adding items

---
+++ 06-05-14/newjersey.php
@@ -0,0 +1,91 @@
+<?php include('../includes/header.inc');	?>
+<meta name="description" content="Cheap new and used furniture. Delaware location. AFR Furniture Clearance Center in Pennsauken, NJ. ">
+
+<h1>Featured new and used furniture discounts in our Pennsauken NJ store</h1>
+<p>DISCLAIMER: Styles and quantities change daily &ndash;<br />
+  Inventory and pricing may vary in each used furniture clearance center location including our Pennsauken New Jersey store.</p>
+<table width="100%" border="0" cellspacing="1" cellpadding="5">
+  <tr>
+    <td background="#ececec"><img src="../images/button-leathersofa.png" width="300" /></td>
+    <td valign="top" background="#ececec"><h2>White sofa and 2 end tables $399</h2></td>
+  </tr>
+  <tr>
+    <td background="#ececec"><img src="../images/strippedsofa.png" width="300" /></td>
+    <td valign="top" background="#ececec"><h2>New! Factory direct sofa and chair, special price $699</h2></td>
+  </tr>
+  <tr>
+    <td background="#ececec"><img src="../images/brownsuede.png" width="300" /></td>
+    <td valign="top" background="#ececec"><h2>Sofa and 2 chairs $399</h2></td>
+  </tr>
+  <tr>
+    <td background="#ececec"><img src="../images/Penndresser.png" width="300" /></td>
+    <td valign="top" background="#ececec"><h2>Wood  dresser and mirror $399</h2></td>
+  </tr>
+  <tr>
+    <td background="#ececec"><img src="../images/Penntable.png" width="300" /></td>
+    <td valign="top" background="#ececec"><h2>5 pc. dinette $199</h2></td>
+  </tr>
+<tr>
+     <td background="#ececec"><img src="../images/052914-4.jpg" width="300" height="225" /></td>
+     <td valign="top" background="#ececec"><h2>Cocktail table with ice chest $179</h2></td>
+</tr>
+<tr>
+     <td background="#ececec"><img src="../images/052914-2.jpg" width="300" height="400" /></td>
+     <td valign="top" background="#ececec"><h2>White chair with table $249</h2></td>
+</tr>
+<tr>
+     <td background="#ececec"><img src="../images/052914-5.jpg" width="300" height="400" /></td>
+     <td valign="top" background="#ececec"><h2>Accent chest $359</h2></td>
+</tr>
+<tr>
+     <td background="#ececec"><img src="../images/052014-6.jpg" width="300" height="225" /></td>
+     <td valign="top" background="#ececec"><h2>5 pc. living room, sofa, love, cocktail and 2 ends $499</h2></td>
+</tr>
+<tr>
+     <td background="#ececec"><img src="../images/051414-4.jpg" width="300" height="225" /></td>
+     <td valign="top" background="#ececec"><h2>Charcoal or red swivel chairs      $149 each</h2></td>
+</tr>
+<tr>
+     <td background="#ececec"><img src="../images/051414-1.jpg" width="300" height="400" /></td>
+     <td valign="top" background="#ececec"><h2>New futon bunk bed with futon mattress $399</h2></td>
+</tr>
+
+  <tr>
+     <td background="#ececec"><img src="../images/042314-1.jpg" width="300" height="225" /></td>
+     <td valign="top" background="#ececec"><h2> Office desk starting at $149</h2></td>
+  </tr>
+  <tr>
+     <td background="#ececec"><img src="../images/042314-3.jpg" width="300" height="225" /></td>
+     <td valign="top" background="#ececec"><h2> Enter to win flat screen 19 inch TV</h2></td>
+  </tr>
+<tr>
+    <td width="300" background="#ececec"><img src="../images/21-piece.jpg" width="300" height="500" /></td>
+    <td valign="top" background="#ececec"><h2>Just in, 21 piece Furniture Package For $1099.00</h2>
+        <ol>
+          <li>Sofa / love, or chair</li>
+          <li>Coffee table  And Two end tables</li>
+          <li>Dinette table with four chairs</li>
+          <li>Dresser, mirror, headboard, and  two night stand&rsquo;s</li>
+          <li>Queen Mattress, Box , and frame</li>
+          <li>Two lamps</li>
+          <li>TV stand</li>
+        </ol></td>
+   </tr>
+   
+   <tr>
+     <td background="#ececec"><img src="../images/NJ-5pc-2.jpg" alt="" width="300" height="210" /></td>
+     <td valign="top" background="#ececec"><h2>Dixon 5 pc bedroom blow out price $199</h2></td>
+   </tr>
+
+  <tr>
+    <td background="#ececec">&nbsp;</td>
+    <td valign="top" background="#ececec"><h4>DISCLAIMER: Styles and quantities change daily &ndash;<br />
+    Inventory and pricing may vary in each location.</h4></td>
+  </tr>
+</table>
+
+<p>&nbsp;</p>
+
+
+<?php include('../includes/footer.inc');	?>
+
